6f1fb1adbe192e44313b8427e299a5a5069be9a5
Josh Sun <joshuasun1998@icloud.com>
2018-02-19 22:14:15 -0800

    ml notes

---
+++ javascript/stdlib/rube8262/test/functions/__main__.js
@@ -0,0 +1,15 @@
+const lib = require('lib');
+
+/**
+* A basic Hello World function
+* @param {string} name Who you're saying hello to
+* @returns {string}
+*/
+module.exports = (name = 'world', context, callback) => {
+
+  callback(null, `hello ${name}`);
+
+};
+
+//messagebird
+//slack app
