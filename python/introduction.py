206e006b183e5f12a911ed2c14153af4bc781fdd
Josh Sun <joshuasun1998@icloud.com>
2018-03-04 15:54:32 -0800

    python

---
+++ python/introduction.py
@@ -0,0 +1,30 @@
+# this is a comment 
+'''
+this is a multiline comment ('triple quotes string');
+'''
+
+# variables
+floatingResult = 17.0 / 4 + 3.8
+print(floatingResult)
+
+# strings
+# can use both "" and ''
+
+aString = "spam"
+anotherString = 'haha'
+print(aString + " " + anotherString)
+
+# multi-line string
+# note: eof characters are automatically added at the end of each line
+print("""
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+
+
+# strings can be concatenated:
+print('py' 'thon');
+
+# slicing
+
